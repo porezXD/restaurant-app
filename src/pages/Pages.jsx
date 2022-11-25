514a8ee552366a0d465363e14928de37c663028e
Jakub M <65038024+porezXD@users.noreply.github.com>
2022-11-26 00:17:04 +0100

    first commit

---
+++ src/pages/Pages.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Cuisine from './Cuisine';
+import Home from "./Home";
+import Recipe from './Recipe';
+import Searched from './Searched';
+import { Route, Routes } from 'react-router-dom';
+
+
+
+function Pages() {
+  return (
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cuisine/:type' element={<Cuisine />} />
+        <Route path='/searched/:search' element={<Searched />} />
+        <Route path='/recipe/:name' element={<Recipe />} />
+      </Routes>
+  )
+}
+
+
+
+
+export default Pages
